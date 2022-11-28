80d6e1f8ec25a0f79b8b9d09e6203d6d59292d9d
Alexandre Conte <alexandre.conte10@gmail.com>
2022-11-28 12:27:12 -0300

    Final version exercise

---
+++ FixacaoVet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FixacaoVet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] estudante = new Estudante[10];
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Rent #{i}");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.WriteLine("Room: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                estudante[roomNumber] = new Estudante(name, email);
+            }
+
+            Console.WriteLine("Busy rooms:");
+            for(int i = 0; i < 10; i++)
+            {
+                if (estudante[i] != null)
+                {
+                    Console.WriteLine(i + ": " + estudante[i]);
+                }
+            }
+        }
+    }
+}
